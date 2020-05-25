1ff2b41d3047ef5e3a38977690e478d3fc1c6553
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-25 17:43:59 -0600

    Create administrator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministratorComponent } from './administrator/administrator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministratorComponent
   ],
   imports: [
     BrowserModule,
