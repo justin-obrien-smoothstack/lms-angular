64729c309210a8e5d64f0762d6be69f9569b0bfa
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-25 18:21:13 -0600

    Apply Prettier formatting to various files

--- src/app/administrator/administrator.component.ts
+++ src/app/administrator/administrator.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-administrator',
-  templateUrl: './administrator.component.html',
-  styleUrls: ['./administrator.component.css']
+  selector: "app-administrator",
+  templateUrl: "./administrator.component.html",
+  styleUrls: ["./administrator.component.css"],
 })
 export class AdministratorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AdministratorComponent } from './administrator/administrator.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AdministratorComponent } from "./administrator/administrator.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdministratorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AdministratorComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
