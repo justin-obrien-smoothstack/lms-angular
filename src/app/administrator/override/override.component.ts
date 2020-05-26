dbd04cce1cd864d9b559fedee69de579c68a0f12
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-26 12:23:51 -0600

    Start adding needed things to OverrideComponent

--- src/app/administrator/override/override.component.ts
+++ src/app/administrator/override/override.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-override',
-  templateUrl: './override.component.html',
-  styleUrls: ['./override.component.css']
+  selector: "app-override",
+  templateUrl: "./override.component.html",
+  styleUrls: ["./override.component.css"],
 })
 export class OverrideComponent implements OnInit {
+  overridableLoans: any;
 
-  constructor() { }
+  constructor(private overrideService: OverrideService) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
