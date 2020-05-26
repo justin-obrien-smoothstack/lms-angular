bcece1788826cebbcaa6ae3fd6959e81c855c213
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-25 20:54:58 -0600

    Add routing for override & link to it from administrator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { AdministratorComponent } from "./administrator/administrator.component";
+import { OverrideComponent } from "./administrator/override/override.component";
 
 const routes: Routes = [
   { path: "lms/administrator", component: AdministratorComponent },
+  { path: "lms/administrator/loan", component: OverrideComponent },
 ];
 
 @NgModule({
