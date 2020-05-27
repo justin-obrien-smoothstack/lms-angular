16940fdafcc7c3c20fc6fc4ae7a8f4ee8a11758e
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-05-27 13:25:03 -0500

    add home, error and layout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from "@angular/core";
+import { LayoutComponent } from "./layout/layout.component";
+import { HomeComponent } from "./home/home.component";
+import { ErrorComponent } from "./error/error.component";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "lms/home",
+        pathMatch: "full",
+      },
+      {
+        path: "lms/home",
+        component: HomeComponent,
+      },
+      {
+        path: "**",
+        component: ErrorComponent,
+      },
+    ],
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ErrorComponentComponent } from './error-component/error-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ErrorComponentComponent, HomeComponentComponent, ErrorComponent, HomeComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
