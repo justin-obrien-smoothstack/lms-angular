beaed6449af2f4dd86e747f3d90d2d1df5c7a07b
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-05-27 14:34:32 -0500

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ErrorComponentComponent } from './error-component/error-component.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, ErrorComponentComponent, HomeComponentComponent, ErrorComponent, HomeComponent, LayoutComponent],
+  declarations: [AppComponent,  ErrorComponent, HomeComponent, LayoutComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
