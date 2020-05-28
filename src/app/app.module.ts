c58bbdc8aaf546b8a0b323e968088c8bf2caf38a
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-05-28 17:07:23 -0500

    update the import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HomeComponent } from "./home/home.component";
 import { LayoutComponent } from "./layout/layout.component";
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
+import { AdminAuthorComponent } from "./administrator/admin-author/admin-author.component";
+import { AdminBookComponent } from "./administrator/admin-book/admin-book.component";
+import { AdminBorrowerComponent } from "./administrator/admin-borrower/admin-borrower.component";
+import { AdminBranchComponent } from "./administrator/admin-branch/admin-branch.component";
+import { AdminGenreComponent } from "./administrator/admin-genre/admin-genre.component";
+import { AdminPublisherComponent } from "./administrator/admin-publisher/admin-publisher.component";
 import { LibrarianComponent } from "./librarian/librarian.component";
 import { BorrowerComponent } from "./borrower/borrower.component";
 import { AdministratorComponent } from "./administrator/administrator.component";
@@ -25,6 +31,12 @@ import { OverrideComponent } from "./administrator/override/override.component";
     BorrowerComponent,
     AdministratorComponent,
     OverrideComponent,
+    AdminAuthorComponent,
+    AdminBookComponent,
+    AdminBorrowerComponent,
+    AdminBranchComponent,
+    AdminGenreComponent,
+    AdminPublisherComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
