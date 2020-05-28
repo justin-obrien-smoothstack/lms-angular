fc4cd5cab9f1902d4b9bb9499df8132dd3019aaa
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-05-28 13:08:51 -0500

    Home page and error page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ErrorComponent } from './error/error.component';
-import { HomeComponent } from './home/home.component';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { ErrorComponent } from "./error/error.component";
+import { HomeComponent } from "./home/home.component";
+import { LayoutComponent } from "./layout/layout.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent,  ErrorComponent, HomeComponent, LayoutComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    ErrorComponent,
+    HomeComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,23 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  over(){
-    console.log("Mouseover called");
-  }
-
-  out(){
-    console.log("Mouseout called");
-  }
-
+  ngOnInit() {}
 }
