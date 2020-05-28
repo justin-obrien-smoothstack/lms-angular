bf4eea999b374400e124c694ae5817269814e2f6
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-27 21:07:09 -0600

    Add OLmsService

---
+++ src/app/common/o/services/oLms.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OLmsService {
+  constructor(private httpClient: HttpClient) {}
+
+  get(url: string) {
+    return this.httpClient.get(url);
+  }
+
+  put(url: string, body: any = "", options: object = {}) {
+    return this.httpClient.put(url, body, options);
+  }
+}
