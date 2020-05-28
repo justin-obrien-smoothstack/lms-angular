601f9863603d7a8ba2c6cde15bcd94e3a0bf2ea2
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-27 21:21:36 -0600

    Add generally required things to app module
    HttpClientModule, OLmsService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AdministratorComponent } from "./administrator/administrator.component";
+import { OLmsService } from "src/app/common/o/services/oLms.service";
 
 @NgModule({
   declarations: [AppComponent, AdministratorComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [OLmsService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
