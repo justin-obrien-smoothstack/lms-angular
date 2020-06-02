877269e4d6a02e5a9aec5b084a4f8cffe8f51899
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-02 11:40:17 -0600

    Add imports to admin book

--- src/app/administrator/admin-book/admin-book.component.ts
+++ src/app/administrator/admin-book/admin-book.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OLmsService } from "src/app/common/o/services/oLms.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-admin-book',
-  templateUrl: './admin-book.component.html',
-  styleUrls: ['./admin-book.component.css']
+  selector: "app-admin-book",
+  templateUrl: "./admin-book.component.html",
+  styleUrls: ["./admin-book.component.css"],
 })
 export class AdminBookComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
