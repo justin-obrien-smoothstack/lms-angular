0e22406304efe16548b85ec69b8099ebaf9020e7
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-07 14:46:41 -0600

    Delete spec files for non-components

--- src/app/common/o/pipes/nice-spacing.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NiceSpacingPipe } from './nice-spacing.pipe';
-
-describe('NiceSpacingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NiceSpacingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
