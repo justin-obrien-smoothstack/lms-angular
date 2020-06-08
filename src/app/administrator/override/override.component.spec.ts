c2da610ed7546b54e2901440d7dbea94a1129e4a
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-07 20:04:27 -0600

    Write a couple override unit tests
    Coverage is now 89.29%

--- src/app/administrator/override/override.component.spec.ts
+++ src/app/administrator/override/override.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { environment } from "src/environments/environment";
 import { HttpClientModule } from "@angular/common/http";
 import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
+import { of } from "rxjs";
 
 import { OverrideComponent } from "./override.component";
 import { OLmsService } from "src/app/common/o/services/oLms.service";
@@ -9,21 +11,48 @@ import { OLmsService } from "src/app/common/o/services/oLms.service";
 describe("OverrideComponent", () => {
   let component: OverrideComponent;
   let fixture: ComponentFixture<OverrideComponent>;
+  let lmsService: OLmsService;
+  const mockLoans = [
+    {
+      cardNo: 6,
+      branchId: 29,
+      bookId: 14,
+      dateOut: new Date("2020-05-15T22:55:38.000Z"),
+      dueDate: new Date("2020-05-22T22:55:38.000Z"),
+      dateIn: new Date("2020-05-22T22:56:00.000Z"),
+    },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [OverrideComponent],
       imports: [NgbPaginationModule, HttpClientModule],
     }).compileComponents();
+    lmsService = new OLmsService(null);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OverrideComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOn(lmsService, "get").and.returnValue(of(mockLoans));
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should call the backend's override function", () => {
+    spyOn(window, "confirm").and.returnValue(true);
+    spyOn(lmsService, "put").and.returnValue(null);
+    component.doOverride(mockLoans[0]);
+    expect(lmsService.put).toHaveBeenCalled;
+  });
+
+  it("should not call the backend's override function", () => {
+    spyOn(window, "confirm").and.returnValue(false);
+    spyOn(lmsService, "put").and.returnValue(null);
+    component.doOverride(mockLoans[0]);
+    expect(lmsService.put).not.toHaveBeenCalled;
   });
 });
