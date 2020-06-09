3941302a99509d3a34453e5960c06de0254f69e2
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-09 09:08:48 -0600

    Got default tests to pass other than override
    Also added a couple more tests for publisher & book

--- src/app/administrator/admin-book/admin-book.component.spec.ts
+++ src/app/administrator/admin-book/admin-book.component.spec.ts
@@ -1,16 +1,50 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from "@angular/core/testing";
 
-import { AdminBookComponent } from './admin-book.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
-describe('AdminBookComponent', () => {
+import { AdminBookComponent } from "./admin-book.component";
+import { GetPropertyPipe } from "src/app/common/o/pipes/get-property.pipe";
+import { NiceSpacingPipe } from "src/app/common/o/pipes/nice-spacing.pipe";
+
+const mockBooks = [
+  { title: "Mock Title", pubId: null, authorIds: [], genreIds: [] },
+];
+
+export class MockModalRef {
+  result = new Promise((resolve) => resolve(null));
+}
+
+describe("AdminBookComponent", () => {
   let component: AdminBookComponent;
   let fixture: ComponentFixture<AdminBookComponent>;
+  let modalService: NgbModal;
+  let formBuilder: FormBuilder;
+  let mockModalRef = new MockModalRef();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminBookComponent ]
-    })
-    .compileComponents();
+      declarations: [AdminBookComponent, GetPropertyPipe, NiceSpacingPipe],
+      imports: [
+        HttpClientModule,
+        NgbModule,
+        NgbPaginationModule,
+        NgMultiSelectDropDownModule,
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
+    modalService = TestBed.get(NgbModal);
   }));
 
   beforeEach(() => {
@@ -19,7 +53,24 @@ describe('AdminBookComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should open a modal window if book is null", fakeAsync(() => {
+    spyOn(modalService, "open").and.returnValue(mockModalRef);
+    component.openWriteModal("Create", "writePublisherModal", null);
+    tick();
+    expect(modalService.open).toHaveBeenCalled();
+  }));
+
+  it("should open a modal window if book is non-null", fakeAsync(() => {
+    spyOn(modalService, "open").and.returnValue(mockModalRef);
+    component.authors = [];
+    component.genres = [];
+    component.publishers = [];
+    component.openWriteModal("Update", "writePublisherModal", mockBooks[0]);
+    tick();
+    expect(modalService.open).toHaveBeenCalled();
+  }));
 });
--- src/app/administrator/admin-publisher/admin-publisher.component.spec.ts
+++ src/app/administrator/admin-publisher/admin-publisher.component.spec.ts
@@ -1,16 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from "@angular/core/testing";
 
-import { AdminPublisherComponent } from './admin-publisher.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
 
-describe('AdminPublisherComponent', () => {
+import { AdminPublisherComponent } from "./admin-publisher.component";
+import { GetPropertyPipe } from "src/app/common/o/pipes/get-property.pipe";
+import { NiceSpacingPipe } from "src/app/common/o/pipes/nice-spacing.pipe";
+
+const mockPublishers = [
+  {
+    publisherName: "Mock Name",
+    publisherAddress: "Mock Address",
+    publisherPhone: "Mock Phone",
+    bookIds: [],
+  },
+];
+
+export class MockModalRef {
+  result = new Promise((resolve) => resolve(null));
+}
+
+describe("AdminPublisherComponent", () => {
   let component: AdminPublisherComponent;
   let fixture: ComponentFixture<AdminPublisherComponent>;
+  let modalService: NgbModal;
+  let formBuilder: FormBuilder;
+  let mockModalRef = new MockModalRef();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminPublisherComponent ]
-    })
-    .compileComponents();
+      declarations: [AdminPublisherComponent, GetPropertyPipe, NiceSpacingPipe],
+      imports: [
+        HttpClientModule,
+        NgbModule,
+        NgbPaginationModule,
+        NgMultiSelectDropDownModule,
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
+    modalService = TestBed.get(NgbModal);
   }));
 
   beforeEach(() => {
@@ -19,7 +58,26 @@ describe('AdminPublisherComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should open a modal window if publisher is null", fakeAsync(() => {
+    spyOn(modalService, "open").and.returnValue(mockModalRef);
+    component.openWriteModal("Create", "writePublisherModal", null);
+    tick();
+    expect(modalService.open).toHaveBeenCalled();
+  }));
+
+  it("should open a modal window if publisher is non-null", fakeAsync(() => {
+    spyOn(modalService, "open").and.returnValue(mockModalRef);
+    component.books = [];
+    component.openWriteModal(
+      "Update",
+      "writePublisherModal",
+      mockPublishers[0]
+    );
+    tick();
+    expect(modalService.open).toHaveBeenCalled();
+  }));
 });
