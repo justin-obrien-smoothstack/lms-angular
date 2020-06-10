f6f38ccbb387c9c625e8f21f7690725706b83af7
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-10 12:27:14 -0600

    Write nice spacing pipe spec

--- src/app/common/o/pipes/nice-spacing.pipe.spec.ts
+++ src/app/common/o/pipes/nice-spacing.pipe.spec.ts
@@ -1,8 +1,12 @@
-import { NiceSpacingPipe } from './nice-spacing.pipe';
+import { NiceSpacingPipe } from "./nice-spacing.pipe";
 
-describe('NiceSpacingPipe', () => {
-  it('create an instance', () => {
+describe("NiceSpacingPipe", () => {
+  it("create an instance", () => {
     const pipe = new NiceSpacingPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it("should transform an array as intended", () => {
+    expect(new NiceSpacingPipe().transform([0, 1])).toEqual("0, 1");
   });
 });
