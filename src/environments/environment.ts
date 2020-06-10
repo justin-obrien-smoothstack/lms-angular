386e9fca4ae169b0afff0fef0af59c16a06b79a8
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-06-10 12:29:16 -0500

    environment clean up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -28,9 +28,6 @@ export const environment = {
   updateBookUri: "/book",
   deleteBookUri: "/books",
   deletePublisherUri: "/publishers",
-  returnBookUri: "/returnBook",
-  checkOutBookUri: "/checkOutBook",
-  copiesUri: "/copies",
 };
 
 /*
